cb38d103164693bc8849907f4d4c11b03c4c8293
ShaplaRani <shaplarani621@gmail.com>
2023-09-11 11:01:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blogs from './conponents/Blogs/Blogs'
+import Header from './conponents/header/header'
+
+function App() {
+
+
+  return (
+    <>
+     
+     <Header></Header>
+     <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
